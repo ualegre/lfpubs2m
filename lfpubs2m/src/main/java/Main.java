e44b19eb830c02424ce60eff95179c9a866e8bee
Unai Alegre <u.alegre@mdx.ac.uk>
2017-04-05 12:59:01 +0100

    Fixed issue with gitignore, renamed examples to LFPUBS output files, made the reference of the input file to a relative path

--- lfpubs2m/src/main/java/Main.java
+++ lfpubs2m/src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		System.out.println("Initializing...");
 		LFPUBS2MTranslator translator = new LFPUBS2MTranslator(true);
 		System.out.println("- - - - - - - - - - - - - - - - - - -");
-		System.out.println(""+translator.getTranslation("/Users/mdx/git/lfpubs2m/lfpubs2m/results/resultLLPUBS.txt"));
+		System.out.println(""+translator.getTranslation("./examples/LFPUBS_Output_0.txt")); //Select an LFPUBS output file
 		System.out.println("- - - - - - - - - - - - - - - - - - -");
 		System.out.println("Ending...");
 	}
